d2a23a52c0bdbf07fc6d3c1e87a982546f705984
suenghun <63846187+suenghun@users.noreply.github.com>
2023-10-28 16:57:51 +0900

    vessl 추가

--- agent/gpo_jssp.py
+++ agent/gpo_jssp.py
@@ -14,7 +14,8 @@ from environment.jssp import Scheduler
 
 # torch.autograd.set_detect_anomaly(True)
 torch.backends.cudnn.benchmark = True
-
+import vessl
+vessl.init()
 # machine, procesing time
 
 def train_model(env, params, log_path=None):
@@ -171,6 +172,7 @@ def train_model(env, params, log_path=None):
         ave_makespan += sum(real_makespan)/params["batch_size"]
 
         if s % params["log_step"] == 0:
+            vessl.log(step=s, payload={'makespan': ave_makespan / (s + 1)})
             t2 = time()
             print('step:%d/%d, actic loss:%1.3f, crictic loss:%1.3f, L:%1.3f, %dmin%dsec' % (s, params["step"], ave_act_loss / ((s + 1) * params["iteration"]),ave_cri_loss / ((s + 1) * params["iteration"]), ave_makespan / (s + 1), (t2 - t1) // 60,(t2 - t1) % 60))
             if log_path is None:
