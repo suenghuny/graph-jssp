9a3584c8ae231d4608ae8620ca69f18c888f7294
suenghun <63846187+suenghun@users.noreply.github.com>
2023-10-28 17:41:57 +0900

    hyp2

--- gpo_jssp.py
+++ gpo_jssp.py
@@ -236,9 +236,9 @@ if __name__ == '__main__':
         "lr_decay_step": 2000,
         "load_model": load_model,
         "gnn": True,
-        "layers":[128, 96 ,64],
-        "n_embedding": 56,
-        "graph_embedding_size" : 32
+        "layers":[196, 128 , 96],
+        "n_embedding": 64,
+        "graph_embedding_size" : 48
     }
 
     #env = PanelBlockShop(params["num_of_process"], params["num_of_blocks"], distribution="lognormal")
