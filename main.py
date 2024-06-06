d81b90fd0c319c5081f983ad2a152f804356da87
suenghun <63846187+suenghun@users.noreply.github.com>
2024-06-06 18:33:01 +0900

    vessl hyp 튜닝

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ from actor2 import PtrNet1
 from jssp2 import AdaptiveScheduler
 from cfg import get_cfg
 import random
+
 cfg = get_cfg()
 baseline_reset = cfg.baseline_reset
 ###
 
 if cfg.vessl == True:
     import vessl
+
     vessl.init()
+
 
 def set_seed(seed):
     random.seed(seed)
@@ -30,13 +33,14 @@ def set_seed(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
+
 # Example usage:
-set_seed(20) # 30 했었음
+set_seed(20)  # 30 했었음
 opt_list = [1059, 888, 1005, 1005, 887, 1010, 397, 899, 934, 944]
 orb_list = []
-for i in ["21","22"]:
+for i in ["21", "22"]:
     df = pd.read_excel("ta.xlsx", sheet_name=i, engine='openpyxl')
-    orb_data = list()#
+    orb_data = list()  #
     for row, column in df.iterrows():
         job = []
         for j in range(0, len(column.tolist()), 2):
@@ -64,7 +68,7 @@ def generate_jssp_instance(num_jobs, num_machine, batch_size):
     return jobs_datas, scheduler_list
 
 
-def heuristic_eval(p): # 불필요(현재로써는)
+def heuristic_eval(p):  # 불필요(현재로써는)
     scheduler = AdaptiveScheduler(orb_list[p - 1])
     makespan_heu = scheduler.heuristic_run()
     return makespan_heu
@@ -81,8 +85,8 @@ def evaluation(act_model, baseline_model, p, eval_number, device, upperbound=Non
     scheduler = AdaptiveScheduler(orb_list[p - 1])  # scheduler는 validation(ORB set)에 대해 수행
 
     num_job = scheduler.num_job
-    num_machine =scheduler.num_mc
-    #print(num_job, num_machine)
+    num_machine = scheduler.num_mc
+    # print(num_job, num_machine)
 
     node_feature = scheduler.get_node_feature()
     node_feature = [node_feature for _ in range(int(eval_number))]
@@ -97,12 +101,12 @@ def evaluation(act_model, baseline_model, p, eval_number, device, upperbound=Non
                                     scheduler_list=scheduler_list_val,
                                     num_machine=num_machine,
                                     num_job=num_job,
-                                    upperbound = upperbound)
+                                    upperbound=upperbound)
     for sequence in pred_seq:
         scheduler = AdaptiveScheduler(orb_list[p - 1])
         makespan = scheduler.run(sequence.tolist())
         val_makespan.append(makespan)
-    #print("크크크", val_makespan)
+    # print("크크크", val_makespan)
     return np.min(val_makespan), np.mean(val_makespan)
 
 
@@ -120,13 +124,13 @@ def train_model(params, log_path=None):
 
     act_model = PtrNet1(params).to(device)
 
-    baseline_model = PtrNet1(params).to(device) # baseline_model 불필요
-    baseline_model.load_state_dict(act_model.state_dict()) # baseline_model 불필요
+    baseline_model = PtrNet1(params).to(device)  # baseline_model 불필요
+    baseline_model.load_state_dict(act_model.state_dict())  # baseline_model 불필요
     if params["optimizer"] == 'Adam':
         act_optim = optim.Adam(act_model.parameters(), lr=params["lr"])
         """
         act_model이라는 신경망 뭉치에 파라미터(가중치, 편향)을 업데이트 할꺼야.
-         
+
         """
 
     elif params["optimizer"] == "RMSProp":
@@ -146,7 +150,7 @@ def train_model(params, log_path=None):
     for s in range(epoch + 1, params["step"]):
 
         """
-        
+
         변수별 shape 
         inputs : batch_size X number_of_blocks X number_of_process
         pred_seq : batch_size X number_of_blocks
@@ -154,53 +158,70 @@ def train_model(params, log_path=None):
         """
         b += 1
 
-        if s % 100 == 1: # Evaluation 수행
+        if s % 100 == 1:  # Evaluation 수행
             for p in problem_list:
                 min_makespan = heuristic_eval(p)
-                eval_number = 10
+                eval_number = 5
                 min_makespan_list = [min_makespan] * eval_number
-                min_makespan1, mean_makespan1 = evaluation(act_model, baseline_model, p, eval_number, device, upperbound=min_makespan_list)
+                min_makespan1, mean_makespan1 = evaluation(act_model, baseline_model, p, eval_number, device,
+                                                           upperbound=min_makespan_list)
 
-                eval_number = 10
+                eval_number = 5
                 min_makespan_list = [min_makespan] * eval_number
-                min_makespan2, mean_makespan2 = evaluation(act_model, baseline_model, p, eval_number, device,upperbound=min_makespan_list)
+                min_makespan2, mean_makespan2 = evaluation(act_model, baseline_model, p, eval_number, device,
+                                                           upperbound=min_makespan_list)
 
-                eval_number = 10
+                eval_number = 5
                 min_makespan_list = [min_makespan] * eval_number
-                min_makespan3, mean_makespan3 = evaluation(act_model, baseline_model, p, eval_number, device,upperbound=min_makespan_list)
+                min_makespan3, mean_makespan3 = evaluation(act_model, baseline_model, p, eval_number, device,
+                                                           upperbound=min_makespan_list)
 
+                eval_number = 5
+                min_makespan_list = [min_makespan] * eval_number
+                min_makespan4, mean_makespan4 = evaluation(act_model, baseline_model, p, eval_number, device,
+                                                           upperbound=min_makespan_list)
 
-                min_makespan = np.min([min_makespan1, min_makespan2, min_makespan3])
-                mean_makespan = (mean_makespan1 + mean_makespan2 + mean_makespan3)/3
+                eval_number = 5
+                min_makespan_list = [min_makespan] * eval_number
+                min_makespan5, mean_makespan5 = evaluation(act_model, baseline_model, p, eval_number, device,
+                                                           upperbound=min_makespan_list)
 
+                eval_number = 5
+                min_makespan_list = [min_makespan] * eval_number
+                min_makespan6, mean_makespan6 = evaluation(act_model, baseline_model, p, eval_number, device,
+                                                           upperbound=min_makespan_list)
 
-                print("TA{}".format(problem_list[p-1]), min_makespan, mean_makespan)
+                min_makespan = np.min([min_makespan1, min_makespan2, min_makespan3,min_makespan4,min_makespan5,min_makespan6])
+                mean_makespan = (mean_makespan1 + mean_makespan2 + mean_makespan3+ mean_makespan4+ mean_makespan5+ mean_makespan6) / 6
+
+                print("TA{}".format(problem_list[p - 1]), min_makespan, mean_makespan)
                 if cfg.vessl == True:
-                    vessl.log(step=s, payload={'minmakespan{}'.format(str(problem_list[p-1])): min_makespan})
-                    vessl.log(step=s, payload={'meanmakespan{}'.format(str(problem_list[p-1])): mean_makespan})
+                    vessl.log(step=s, payload={'minmakespan{}'.format(str(problem_list[p - 1])): min_makespan})
+                    vessl.log(step=s, payload={'meanmakespan{}'.format(str(problem_list[p - 1])): mean_makespan})
                 else:
-                    validation_records_min[p-1].append(min_makespan)
-                    validation_records_mean[p-1].append(mean_makespan)
+                    validation_records_min[p - 1].append(min_makespan)
+                    validation_records_mean[p - 1].append(mean_makespan)
                     min_m = pd.DataFrame(validation_records_min)
                     mean_m = pd.DataFrame(validation_records_mean)
                     min_m = min_m.transpose()
                     mean_m = mean_m.transpose()
                     min_m.columns = problem_list
                     mean_m.columns = problem_list
-                    min_m.to_csv('min_makespan_wo_bnb.csv')
-                    mean_m.to_csv('mean_makespan_wo_bnb.csv')
+                    min_m.to_csv('min_makespan_w_third_feature2.csv')
+                    mean_m.to_csv('mean_makespan_w_third_feature2.csv')
 
         act_model.block_indices = []
         baseline_model.block_indices = []
 
-        if s % cfg.gen_step == 1:                            # 훈련용 Data Instance 새롭게 생성 (gen_step 마다 생성)
+        if s % cfg.gen_step == 1:  # 훈련용 Data Instance 새롭게 생성 (gen_step 마다 생성)
             """
-            
+
             훈련용 데이터셋 생성하는 코드
             """
             num_job = np.random.randint(5, 10)
             num_machine = np.random.randint(num_job, 10)
-            jobs_datas, scheduler_list = generate_jssp_instance(num_jobs=num_job,  num_machine=num_machine, batch_size=params['batch_size'])
+            jobs_datas, scheduler_list = generate_jssp_instance(num_jobs=num_job, num_machine=num_machine,
+                                                                batch_size=params['batch_size'])
             makespan_list_for_upperbound = list()
             for scheduler in scheduler_list:
                 c_max_heu = scheduler.heuristic_run()
@@ -210,9 +231,9 @@ def train_model(params, log_path=None):
             for scheduler in scheduler_list:
                 scheduler.reset()
         """
-        
+
         """
-        act_model.get_jssp_instance(scheduler_list) # 훈련해야할 instance를 에이전트가 참조(등록)하는 코드
+        act_model.get_jssp_instance(scheduler_list)  # 훈련해야할 instance를 에이전트가 참조(등록)하는 코드
         heterogeneous_edges = list()
         node_features = list()
 
@@ -223,25 +244,23 @@ def train_model(params, log_path=None):
             - Edge: 
             """
 
-
             scheduler = AdaptiveScheduler(jobs_datas[n])
             node_feature = scheduler.get_node_feature()
             node_features.append(node_feature)
             edge_precedence = scheduler.get_edge_index_precedence()
             edge_antiprecedence = scheduler.get_edge_index_antiprecedence()
             edge_machine_sharing = scheduler.get_machine_sharing_edge_index()
-            heterogeneous_edge = (edge_precedence, edge_antiprecedence, edge_machine_sharing) # 세종류의 엑지들을 하나의 변수로 참조시킴
+            heterogeneous_edge = (edge_precedence, edge_antiprecedence, edge_machine_sharing)  # 세종류의 엑지들을 하나의 변수로 참조시킴
             heterogeneous_edges.append(heterogeneous_edge)
         input_data = (node_features, heterogeneous_edges)
         act_model.train()
         pred_seq, ll_old, _ = act_model(input_data, device, scheduler_list=scheduler_list,
                                         num_machine=num_machine, num_job=num_job,
-                                        upperbound = makespan_list_for_upperbound
-                                            )
-
+                                        upperbound=makespan_list_for_upperbound
+                                        )
 
         real_makespan = list()
-        for n in range(pred_seq.shape[0]): # act_model(agent)가 산출한 해를 평가하는 부분
+        for n in range(pred_seq.shape[0]):  # act_model(agent)가 산출한 해를 평가하는 부분
             sequence = pred_seq[n]
             scheduler = AdaptiveScheduler(jobs_datas[n])
             makespan = -scheduler.run(sequence.tolist()) / params['reward_scaler']
@@ -253,15 +272,15 @@ def train_model(params, log_path=None):
         """
         if baseline_reset == False:
             if s == 1:
-                be = torch.tensor(real_makespan).detach().unsqueeze(1).to(device)             #  baseline을 구하는 부분
+                be = torch.tensor(real_makespan).detach().unsqueeze(1).to(device)  # baseline을 구하는 부분
             else:
                 be = cfg.beta * be + (1 - cfg.beta) * torch.tensor(real_makespan).to(device)
         else:
             if s % cfg.gen_step == 1:
-                be = torch.tensor(real_makespan).detach().unsqueeze(1).to(device)             #  baseline을 구하는 부분
+                be = torch.tensor(real_makespan).detach().unsqueeze(1).to(device)  # baseline을 구하는 부분
             else:
                 be = cfg.beta * be + (1 - cfg.beta) * torch.tensor(real_makespan).to(device)
-
+        ####
         act_optim.zero_grad()
         adv = torch.tensor(real_makespan).detach().unsqueeze(1).to(device) - be  # baseline(advantage) 구하는 부분
         """
@@ -270,11 +289,7 @@ def train_model(params, log_path=None):
         3. Update 하기(act_optim.step)
         """
 
-
-
-
-
-        act_loss = -(ll_old * adv).mean()                                        # loss 구하는 부분 /  ll_old의 의미 log_theta (pi | s)
+        act_loss = -(ll_old * adv).mean()  # loss 구하는 부분 /  ll_old의 의미 log_theta (pi | s)
         act_loss.backward()
         act_optim.step()
         nn.utils.clip_grad_norm_(act_model.parameters(), max_norm=10.0, norm_type=2)
@@ -309,9 +324,8 @@ def train_model(params, log_path=None):
                         'ave_act_loss': ave_act_loss,
                         'ave_cri_loss': 0,
                         'ave_makespan': ave_makespan},
-                       params["model_dir"] + '/ppo_bnb' + '/%s_step%d_act.pt' % (date, s))
+                       params["model_dir"] + '/ppo_w_third_feature' + '/%s_step%d_act.pt' % (date, s))
         #     print('save model...')
-
 
 
 if __name__ == '__main__':
@@ -323,11 +337,11 @@ if __name__ == '__main__':
         os.makedirs(log_dir + "/ppo")
 
     model_dir = "./result/model"
-    if not os.path.exists(model_dir + "/ppo_bnb"):
-        os.makedirs(model_dir + "/ppo_bnb")
+    if not os.path.exists(model_dir + "/ppo_w_third_feature"):
+        os.makedirs(model_dir + "/ppo_w_third_feature")
 
     params = {
-        "num_of_process": 5,
+        "num_of_process": 6,
         "step": cfg.step,
         "log_step": cfg.log_step,
         "log_dir": log_dir,
@@ -345,23 +359,23 @@ if __name__ == '__main__':
         "optimizer": "Adam",
         "n_glimpse": cfg.n_glimpse,
         "n_process": cfg.n_process,
-        "lr": cfg.lr,
+        "lr": float(os.environ.get("lr", 1e-3)),
         "is_lr_decay": cfg.is_lr_decay,
-        "lr_decay": cfg.lr_decay,
-        "lr_decay_step": cfg.lr_decay_step,
+        "lr_decay": float(os.environ.get("lr_decay",0.995)),
+        "lr_decay_step": int(os.environ.get("lr_decay_step", 1000)),
         "lr_decay_step_critic": cfg.lr_decay_step_critic,
         "load_model": load_model,
-        "layers": eval(str(os.environ.get("layers", '[128, 64]'))),
+        "layers": eval(str(os.environ.get("layers", '[256, 128]'))),
         "lr_critic": cfg.lr_critic,
         "n_embedding": int(os.environ.get("n_embedding", 32)),
         "graph_embedding_size": int(os.environ.get("graph_embedding_size", 64)),
         "reward_scaler": cfg.reward_scaler,
-        "n_multi_head": int(os.environ.get("n_multi_head", 4)),
+        "n_multi_head": int(os.environ.get("n_multi_head", 2)),
         "entropy_weight": cfg.entropy_weight,
         "dot_product": cfg.dot_product,
-        "third_feature": cfg.third_feature,
+        "third_feature": True,
         "baseline_reset": cfg.baseline_reset,
-        "feature_critical_path": cfg.feature_critical_path
+        "ex_embedding": True
     }
 
     train_model(params)
