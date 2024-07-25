52a28cb0298562ee781e2c911c5ea372689d060f
suenghun <63846187+suenghun@users.noreply.github.com>
2024-07-25 12:15:02 +0900

    데이터 변경 한번만 해보자

--- ppo_debug2.py
+++ ppo_debug2.py
@@ -231,8 +231,8 @@ def train_model(params, log_path=None):
             """
             훈련용 데이터셋 생성하는 코드
             """
-            num_job = np.random.randint(5, 10)
-            num_machine = np.random.randint(num_job, 10)
+            num_machine = np.random.randint(5, 11)
+            num_job = np.random.randint(num_machine, 11)
 
 
             jobs_datas, scheduler_list = generate_jssp_instance(num_jobs=num_job, num_machine=num_machine,
