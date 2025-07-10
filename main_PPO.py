acdf73849f198ad724cd53580e92a2efc5f2dcca
suenghuny <suenghuny@naver.com>
2025-07-10 20:19:25 +0900

    param6 추가

--- main_PPO.py
+++ main_PPO.py
@@ -280,7 +280,7 @@ def train_model(params, selected_param, log_path=None):
                             'ave_act_loss': ave_act_loss,
                             'ave_cri_loss': 0,
                             'ave_makespan': ave_makespan},
-                           params["model_dir"] + '/{}_step_{}_mean_makespan_{}_w_rep.pt'.format(selected_param, s, mean_makespan72))
+                           params["model_dir"] + '/w_rep_{}_step_{}_mean_makespan_{}.pt'.format(selected_param, s, mean_makespan72))
             else:
                 torch.save({'epoch': s,
                             'model_state_dict_actor': act_model.state_dict(),
@@ -288,7 +288,7 @@ def train_model(params, selected_param, log_path=None):
                             'ave_act_loss': ave_act_loss,
                             'ave_cri_loss': 0,
                             'ave_makespan': ave_makespan},
-                           params["model_dir"] + '/{}_step_{}_mean_makespan_{}_wo_rep.pt'.format(selected_param, s, mean_makespan72))
+                           params["model_dir"] + '/wo_rep_{}_step_{}_mean_makespan_{}.pt'.format(selected_param, s, mean_makespan72))
         act_model.block_indices = []
         baseline_model.block_indices = []
 
