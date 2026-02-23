8be974e1dee7f1452af5fc6bc1acb208af2fbd32
suenghuny <suenghuny@naver.com>
2026-02-23 22:47:53 +0900

    param 1 64,128

--- main.py
+++ main.py
@@ -185,7 +185,45 @@ if __name__ == '__main__':
         "graph_embedding_size": 96,
              }
 
+    param1_64 ={
+             "alpha": 0.05,
+             "n_hidden": 64,
+             "ex_embedding_size": 32,
+             "ex_embedding_size2": 54,
+             "n_multi_head": 1,
+             "k_hop": 1,
+             "lr_latent":  1.0e-4,
+             "lr_critic": 1.0e-4,
+             "lr": 1.0e-4,
+             "entropy_coeff": 0.001,
+        "entropy_min": 0.0,
+             "layers": eval('[196, 128]'),
+             "lr_decay_step": 600,
+             "lr_decay": 0.99,
+             "lr_decay_min": 5e-5,
+            'rep_anneal': 30000,
+        "graph_embedding_size": 96,
+             }
 
+    param1_128 ={
+             "alpha": 0.05,
+             "n_hidden": 128,
+             "ex_embedding_size": 32,
+             "ex_embedding_size2": 54,
+             "n_multi_head": 1,
+             "k_hop": 1,
+             "lr_latent":  1.0e-4,
+             "lr_critic": 1.0e-4,
+             "lr": 1.0e-4,
+             "entropy_coeff": 0.001,
+        "entropy_min": 0.0,
+             "layers": eval('[196, 128]'),
+             "lr_decay_step": 600,
+             "lr_decay": 0.99,
+             "lr_decay_min": 5e-5,
+            'rep_anneal': 30000,
+        "graph_embedding_size": 96,
+             }
 
     selected_param = str(os.environ.get("selected_param", "param2"))
     aggr = str(os.environ.get("aggr", "mean"))
@@ -194,6 +232,8 @@ if __name__ == '__main__':
     param_group = {
                    "param0": param0,
                    "param1": param1,
+                    "param1_64": param1_64,
+                      "param1_128": param1_128,
                    "param2": param2,
                   "param4_64": param4_64,
                    "param4": param4,
